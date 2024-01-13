149d39bcb55d88e25a3dc83dd4e170ccc333ae78
SumirSingh19 <134550739+SumirSingh19@users.noreply.github.com>
2024-01-13 19:40:23 +0530

    Updated from 2nd episode

--- App.js
+++ App.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
-  );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading1"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading1]
-  );
-  
-  // create root using createRoot
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(container);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+    "div", 
+    {id: "parent"},
+    [React.createElement(
+        "div", 
+        {id: "child1"}, 
+        [React.createElement("h1", {}, "First h1"), React.createElement("h2", {id: "parent"}, "Under h1")]
+    ),
+    React.createElement(
+        "div", 
+        {id: "child2"}, 
+        [React.createElement("h1", {}, "First h1"), React.createElement("h2", {id: "parent"}, "Under h1")]
+    ) ]
+);
+
+//The created element by the React gives an object in which all of this HTML code is defined which is then rendered and browser understands
+// in the form of an HTML 
+//ReactElement(Object) => HTML(Browser Understands) 
+
+
+//const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent );
---
+++ README.md
@@ -0,0 +1,20 @@
+# Learning from NamasteReact Course 
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and Production bundles
